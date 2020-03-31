fd2d905cdc5713a86d100c6425ac1810673e99c5
Lauri Korts-Pärn <lauri@korc.jp>
2020-03-31 12:11:13 -0400

    make latest check verbose

--- usr/src/sfs.d/.common.sh
+++ usr/src/sfs.d/.common.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-: ${lbu:=/opt/LiveBootUtils}
+: "${lbu:=/opt/LiveBootUtils}"
 . "$lbu/scripts/common.func"
 
-: ${dl_page:=https://golang.org/dl/}
-: ${tgz_fpat:=linux-amd64.tar.gz}
+: "${dl_page:=https://golang.org/dl/}"
+: "${tgz_fpat:=linux-amd64.tar.gz}"
 
 latest_url() {
   curl -s "$dl_page" | grep -o "https://[^\"'[:space:]>]*${tgz_fpat}" | head -1
@@ -15,7 +15,17 @@ installed_ver() {
 }
 
 latest_ver() {
-  : ${tgz_url:=$(latest_url)}
+  : "${tgz_url:=$(latest_url)}"
   local latest_base="$(basename "$tgz_url")"
   echo "${latest_base%.$tgz_fpat}"
+}
+
+is_latest() {
+  local installed_ver="$(installed_ver)"
+  local latest_ver="$(latest_ver)"
+  test "x$(installed_ver)" = "x$(latest_ver)" || {
+    echo "Installed version not latest: '$installed_ver' -> '$latest_ver'" >&2
+    return 1
+  }
+  echo "Latest version installed: '$latest_ver'" >&2
 }
